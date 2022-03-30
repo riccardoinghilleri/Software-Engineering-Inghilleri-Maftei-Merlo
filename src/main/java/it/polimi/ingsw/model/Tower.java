c77764310949763c1cc18b411395d4d6e408787d
ManuMerlo <merlomanu1999@gmail.com>
2022-03-30 22:10:20 +0200

    Added getOwner() method in Tower class

--- src/main/java/it/polimi/ingsw/model/Tower.java
+++ src/main/java/it/polimi/ingsw/model/Tower.java
@@ -4,9 +4,15 @@ import it.polimi.ingsw.model.enums.PlayerColor;
 
 public class Tower {
     private final PlayerColor color;
+    private final String owner;
 
-    public Tower(PlayerColor color) {
+    public Tower(String owner, PlayerColor color) {
+        this.owner=owner;
         this.color = color;
+    }
+
+    public String getOwner() {
+        return owner;
     }
 
     public PlayerColor getColor() {

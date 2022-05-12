353474e267439e8416fc35031319a2e0e917531c
ManuMerlo <merlomanu1999@gmail.com>
2022-05-12 10:31:55 +0200

    Fix Knight for 4 player.

--- src/main/java/it/polimi/ingsw/controller/actioncontroller/Knight.java
+++ src/main/java/it/polimi/ingsw/controller/actioncontroller/Knight.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.controller.actioncontroller;
 
+import it.polimi.ingsw.enums.PlayerColor;
 import it.polimi.ingsw.server.ConnectionMessage.ActionMessage;
 import it.polimi.ingsw.server.model.GameModel;
 import it.polimi.ingsw.server.model.Player;
@@ -15,16 +16,23 @@ public class Knight extends ActionController {
         super(gameModel);
         //TODO da finire
     }
+
     //metodo che calcola l'influenza aggiungenddo due punti addizionali al currentPlayer
     @Override
     public int getInfluence(ActionMessage actionMessage) {
         int[] influence = new int[getGameModel().getPlayers().size()];
-        for(Player player : getGameModel().getPlayers()) {
-            influence[player.getClientID()]=0;
+        for (Player player : getGameModel().getPlayers()) {
+            influence[player.getClientID()] = 0;
         }
-        influence = getGameModel().getBoard().getStudentInfluence(actionMessage.getData(),influence, Arrays.asList(CharacterColor.values()));
-        influence = getGameModel().getBoard().getTowersInfluence(actionMessage.getData(),influence);
-        influence[getGameModel().getCurrentPlayer().getClientID()]+=2;
+        influence = getGameModel().getBoard().getStudentInfluence(actionMessage.getData(), influence, Arrays.asList(CharacterColor.values()));
+        if (getGameModel().getPlayersNumber() < 3)
+            influence[getGameModel().getCurrentPlayer().getClientID()] += 2;
+        else {
+            if (getGameModel().getCurrentPlayer().getColor().equals(PlayerColor.WHITE))
+                influence[0]++;
+            else influence[1]++;
+        }
+        influence = getGameModel().getBoard().getTowersInfluence(actionMessage.getData(), influence);
         return getGameModel().getBoard().getMaxInfluence(influence);
     }
 }
